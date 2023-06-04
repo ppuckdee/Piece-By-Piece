153326a6a63122205dd5331e82aa2e8330e2c386
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2023-06-03 17:26:39 -0700

    enemymove

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SnailMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     public Transform[] patrolPoints;
     public float moveSpeed;
