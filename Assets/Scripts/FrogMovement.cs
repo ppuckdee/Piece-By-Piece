e74becd8a12ccbabb5edafe88e48a415b87d2a27
Nicholas Puckdee <132693105+ppuckdee@users.noreply.github.com>
2023-06-04 19:53:37 -0700

    changes to frog jump movement

--- Assets/Scripts/FrogMovement.cs
+++ Assets/Scripts/FrogMovement.cs
@@ -5,25 +5,59 @@ using UnityEngine;
 public class FrogMovement : MonoBehaviour
 {
     public Transform player; 
-    public float jumpForce = 5f;  
+    public float jumpHeight = 3f;  
+    public bool jumping, grounded;
+    public LayerMask groundCheckMask;
 
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumping = grounded = false;
     }
 
     void Update()
     {
-        if (player.position.y > transform.position.y)
+        checkGrounded(0.05f);
+        if (grounded && player.position.y > transform.position.y)
         {
             Jump();
         }
+        if(jumping && rb.velocity.y < 0) jumping = false;
     }
 
     void Jump()
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        jumping = true;
+        rb.velocity = new Vector2(rb.velocity.x, Mathf.Sqrt(-2*Physics2D.gravity.y*jumpHeight));
+    } 
+
+    private void checkGrounded(float dist)
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        Vector3 ray1Pos = transform.position + new Vector3(collider.offset.x - collider.size.x/2 + 0.05f, collider.offset.y - collider.size.y/2 + 0.005f) * transform.localScale.y;
+        Vector3 ray2Pos = transform.position + new Vector3(collider.offset.x, collider.offset.y - collider.size.y/2 + 0.005f) * transform.localScale.y;
+        Vector3 ray3Pos = transform.position + new Vector3(collider.offset.x + collider.size.x/2 - 0.05f, collider.offset.y - collider.size.y/2 + 0.005f) * transform.localScale.y;
+
+        Debug.DrawRay(ray1Pos, Vector2.down*dist, Color.red);
+        Debug.DrawRay(ray2Pos, Vector2.down*dist, Color.red);
+        Debug.DrawRay(ray3Pos, Vector2.down*dist, Color.red);
+        if(jumping)
+        {
+            grounded = false;
+            return;
+        }
+        RaycastHit2D groundCheckHit1 = Physics2D.Raycast(ray1Pos, Vector3.down, dist, groundCheckMask);
+        RaycastHit2D groundCheckHit2 = Physics2D.Raycast(ray2Pos, Vector3.down, dist, groundCheckMask);
+        RaycastHit2D groundCheckHit3 = Physics2D.Raycast(ray3Pos, Vector3.down, dist, groundCheckMask);
+        if(groundCheckHit1.collider != null || groundCheckHit2.collider != null || groundCheckHit3.collider != null)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
     }
 }
